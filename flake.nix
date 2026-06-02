a0e67a034caafb87860cf32ceee616db251510b8
codywright <acodywright@gmail.com>
2026-06-01 19:18:46 -0700

    chore: repoint git deps from starcommand.live to Codeberg

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    fts-repo.url = "git+https://git.starcommand.live/FastTrackStudios/fts-repo";
+    fts-repo.url = "git+https://codeberg.org/FastTrackStudios/fts-repo";
     fts-repo.inputs.nixpkgs.follows = "nixpkgs";
   };
   nixConfig = {
